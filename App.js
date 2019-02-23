0bcf5fba1809aacf9b0c1c9dd8920295fbeb9d9b
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-23 11:27:02 -0800

    learning use of images and activity indicator

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import Generate from './src/components/Generator/Generator';
 import Input from './src/components/Input/Input';
 import Landing from './src/components/LandingPage/Landing';
 import ListItem from './src/components/Generator/ListItem';
 import Nav from './src/components/Nav/Nav';
 import PickerComponent from './src/components/Picker/Picker';
+import Wingsuit from './assets/images/wingsuit.jpg';
 
 class App extends Component {
 
   state = {
     appName: 'Secret Spot',
     random: [2, 93],
+    loading: false,
   }
 
   onAddRandom = () => {
@@ -48,7 +50,18 @@ class App extends Component {
             <ListItem items={this.state.random} delete={this.onItemDelete} />
 
             {/* <Input /> */}
-            <PickerComponent />
+            {/* <PickerComponent />
+            <ActivityIndicator 
+              size="large"
+              color="purple"
+              animating={this.state.loading}
+            /> */}
+            <Image 
+              source={{uri: 'https://images.vailresorts.com/image/upload/c_scale,dpr_1.0,f_auto,q_auto,w_1000/v1/Whistler%20Blackcomb/Products/Home/Whistler%20Homepage%20Photo%20Image%20Promotion%203.png'}}
+              style={styles.wingsuit}
+              resizeMode="contain"
+              onLoadEnd={() => alert('Image loaded')}
+            />
           </View>
         </ScrollView>
       </View>
@@ -62,6 +75,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'flex-start',
+    marginTop: 20,
+  },
+  wingsuit: {
+    width: '100%',
+    height: 300,
     marginTop: 20,
   },
   wrapper: {
