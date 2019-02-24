3d59b5171b20b177b58b6f8edf58f75efc49b670
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2019-02-24 11:13:51 -0800

    practiced with natives navigation; going to set up actually project

--- App.js
+++ App.js
@@ -1,13 +1,6 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import Generate from './src/components/Generator/Generator';
-import Input from './src/components/Input/Input';
-import Landing from './src/components/LandingPage/Landing';
-import ListItem from './src/components/Generator/ListItem';
-import ModalComponent from './src/components/Modal/Modal';
-import Nav from './src/components/Nav/Nav';
-import PickerComponent from './src/components/Picker/Picker';
-import Wingsuit from './assets/images/wingsuit.jpg';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 class App extends Component {
 
@@ -38,35 +31,7 @@ class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Nav appName={this.state.appName} />
-        <ScrollView 
-          style={{width: '100%'}}
-          // onContentSizeChange={(w,h) => alert(h)}
-          // onMomentumScrollBegin={()=> alert('begin')}
-          // onMomentumScrollEnd={()=> alert('end')}
-          // onScroll={() => alert('scrollling')}
-        >
-          <View style={styles.wrapper}>
-            <Generate add={this.onAddRandom} />
-            <ListItem items={this.state.random} delete={this.onItemDelete} />
-
-            {/* <Input /> */}
-            {/* <PickerComponent />
-            <ActivityIndicator 
-              size="large"
-              color="purple"
-              animating={this.state.loading}
-            /> */}
-            {/* <Image 
-              source={{uri: 'https://images.vailresorts.com/image/upload/c_scale,dpr_1.0,f_auto,q_auto,w_1000/v1/Whistler%20Blackcomb/Products/Home/Whistler%20Homepage%20Photo%20Image%20Promotion%203.png'}}
-              style={styles.wingsuit}
-              resizeMode="contain"
-              onLoadEnd={() => alert('Image loaded')}
-            /> */}
-            <ModalComponent />
-
-          </View>
-        </ScrollView>
+        <Text>My app</Text>
       </View>
     );
   }
@@ -79,18 +44,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start',
     marginTop: 20,
-  },
-  wingsuit: {
-    width: '100%',
-    height: 300,
-    marginTop: 20,
-  },
-  wrapper: {
-    flex: 1,
-    width: '100%',
-    padding: 20,
-    alignItems: 'center',
-    justifyContent: 'flex-start',
   },
 });
 
